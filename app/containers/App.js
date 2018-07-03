2ac6af44ff197c34f2f368da71f2bb4a4440dedf
Janne Hellsten <jjhellst@gmail.com>
2018-07-03 20:36:20 +0300

    fix yarn build problem

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,16 +1,15 @@
 
-import * as React from 'react';
+import React, {Component } from 'react';
 
 import s from './App.css'
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <div className={s.appGrid}>
         <div className={s.empty} />
         <div className={s.topmenu}><h2>PETSCII EDITOR</h2></div>
-        <div className={s.leftmenubar}>
-        </div>
+        <div className={s.leftmenubar} />
         <div className={s.editor}>
           {this.props.children}
         </div>
