e39f442b26f810b4eaa67fdbac7d8a265189a3a4
Janne Hellsten <jjhellst@gmail.com>
2018-07-03 20:59:44 +0300

    simplify dev setup so that we don't need to maintain actions/reducers in store creation for dev mode

--- app/store/configureStore.dev.js
+++ app/store/configureStore.dev.js
@@ -1,10 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
-import { routerMiddleware, routerActions } from 'react-router-redux';
+import { routerMiddleware } from 'react-router-redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
-import * as editorActions from '../actions/editor';
 
 const history = createHashHistory();
 
@@ -30,18 +29,10 @@ const configureStore = (initialState) => {
   // Router Middleware
   const router = routerMiddleware(history);
   middleware.push(router);
-
-  // Redux DevTools Configuration
-  const actionCreators = {
-    ...editorActions,
-    ...routerActions
-  };
   // If Redux DevTools Extension is installed use it, otherwise use Redux compose
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Options: http://zalmoxisus.github.io/redux-devtools-extension/API/Arguments.html
-        actionCreators
       })
     : compose;
   /* eslint-enable no-underscore-dangle */
