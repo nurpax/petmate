231a59228188b084e3e100343d977df48055e234
Janne Hellsten <jjhellst@gmail.com>
2018-07-11 02:58:05 +0300

    automatically close color picker after mouse leave of 500 ms

--- app/containers/Toolbar.js
+++ app/containers/Toolbar.js
@@ -26,21 +26,46 @@ class Icon extends Component {
 }
 
 class FbColorPicker extends Component {
-  state = {
-    colorPickActive: false
+
+  constructor (props) {
+    super(props)
+    this.timerId = null
+  }
+
+  componentWillUnmount () {
+    if (this.timerId !== null) {
+      clearTimeout(this.timerId)
+    }
   }
 
   handleColorPickActive = () => {
-    this.setState((prevState) => {
-      return {
-        colorPickActive: !prevState.colorPickActive
-      }
-    })
+    const newIsActive = !this.props.active
+    this.props.onActivatePicker(this.props.pickerId, newIsActive)
+    if (this.timerId !== null) {
+      this.clearHoverTimer()
+    }
+  }
+
+  clearHoverTimer = () => {
+    if (this.timerId !== null) {
+      clearTimeout(this.timerId)
+      this.timerId = null
+    }
+  }
+
+  handleMouseEnter = () => {
+    this.clearHoverTimer()
+  }
+
+  handleMouseLeave = () => {
+    clearTimeout(this.timerId)
+    this.timerId = setTimeout(() => {
+      this.props.onActivatePicker(this.props.pickerId, false)
+    }, 500)
   }
 
   handleSelectColor = (idx) => {
     this.props.onSelectColor(idx, null)
-    this.setState({colorPickActive: false})
   }
 
   render () {
@@ -51,14 +76,23 @@ class FbColorPicker extends Component {
       backgroundColor: bg,
       flex: 1
     }
-    return (
-      <div className={classnames(styles.tooltip)}>
-        <div style={s} onClick={this.handleColorPickActive} />
-        <div className={classnames(styles.colorpicker, this.state.colorPickActive ? styles.active : null)}>
+    let picker = null
+    if (this.props.active) {
+      picker =
+        <div className={styles.colorpicker}>
           <div style={{transform: 'scale(2,2)', transformOrigin:'0% 0%'}}>
             <ColorPicker color={this.props.color} onSelectColor={this.handleSelectColor} />
           </div>
         </div>
+    }
+    return (
+      <div
+        className={classnames(styles.tooltip)}
+        onMouseLeave={this.handleMouseLeave}
+        onMouseEnter={this.handleMouseEnter}
+      >
+        <div style={s} onClick={this.handleColorPickActive} />
+        {picker}
       </div>
     )
   }
@@ -66,11 +100,31 @@ class FbColorPicker extends Component {
 
 class ToolbarView extends Component {
 
+  state = {
+    colorPickerActive: {
+      border: false,
+      background: false
+    }
+  }
+
+  setColorPickerActive = (pickerId, val) => {
+    this.setState(prevState => {
+      return {
+        colorPickerActive: {
+          ...prevState.colorPickerActive,
+          [pickerId]: val
+        }
+      }
+    })
+  }
+
   handleSelectBgColor = (color) => {
+    this.setColorPickerActive('background', false)
     this.props.Framebuffer.setBackgroundColor(color)
   }
 
   handleSelectBorderColor = (color) => {
+    this.setColorPickerActive('border', false)
     this.props.Framebuffer.setBorderColor(color)
   }
 
@@ -87,11 +141,17 @@ class ToolbarView extends Component {
           onIconClick={this.props.redo}
           iconName='fa-repeat' tooltip='Redo'/>
         <FbColorPicker
+          pickerId='border'
+          active={this.state.colorPickerActive.border}
           color={this.props.borderColor}
+          onActivatePicker={this.setColorPickerActive}
           onSelectColor={this.handleSelectBorderColor}
         />
         <FbColorPicker
+          pickerId='background'
+          active={this.state.colorPickerActive.background}
           color={this.props.backgroundColor}
+          onActivatePicker={this.setColorPickerActive}
           onSelectColor={this.handleSelectBgColor}
         />
       </div>
