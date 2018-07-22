c75c133595105cfe3c931bcdaf489a87fc15f6af
Janne Hellsten <jjhellst@gmail.com>
2018-07-22 22:30:55 +0300

    Show thumbnail of screen contents in tab selector

--- app/containers/FramebufferTabs.js
+++ app/containers/FramebufferTabs.js
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 
 import classnames from 'classnames'
 
+import CharGrid from '../components/CharGrid'
 import styles from './FramebufferTabs.css'
 import { framebufIndexMergeProps }  from '../redux/utils'
 import { Framebuffer } from '../redux/editor'
@@ -12,9 +13,50 @@ import { Toolbar } from '../redux/toolbar'
 import * as framebufList from '../redux/framebufList'
 import * as selectors from '../redux/selectors'
 
+import * as utils from '../utils'
+
+class FramebufTab extends Component {
+  handleSelect = () => {
+    this.props.onSetActiveTab(this.props.id)
+  }
+  render () {
+    const { width, height, framebuf, backgroundColor, borderColor } =
+      this.props.framebuf
+    const backg = utils.colorIndexToCssRgb(backgroundColor)
+    const scaleX = 1.0/4
+    const scaleY = 1.0/4
+    const s = {
+      width: 40*2,
+      height: 25*2,
+      backgroundColor: '#000',
+      marginRight: '3px',
+    }
+    const scaleStyle = {
+      transform: `scale(${scaleX}, ${scaleY})`,
+      transformOrigin: '0% 0%'
+    }
+    return (
+      <div
+        onClick={this.handleSelect}
+        className={classnames(styles.tab, this.props.active ? styles.active : null)}
+        style={s}
+      >
+        <div style={scaleStyle}>
+          <CharGrid
+            width={width}
+            height={height}
+            backgroundColor={backg}
+            grid={false}
+            framebuf={framebuf}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
 class FramebufferTabs_ extends Component {
-  handleActiveClick = (idx) => (e) => {
-    e.preventDefault()
+  handleActiveClick = (idx) => {
     this.props.Toolbar.setFramebufIndex(idx)
   }
 
@@ -35,22 +77,22 @@ class FramebufferTabs_ extends Component {
       const name = `Tab ${t.id}`
       const cls = classnames(i === this.props.framebufIndex ? styles.active : null)
       return (
-        <li key={key} className={cls}>
-          <a href='/#' onClick={this.handleActiveClick(i)}>{name}</a>
-          &nbsp;
-          {disableRemove ?
-            null
-            :
-            <i onClick={() => this.handleRemoveTab(i)} className='fa fa-times-circle'></i>}
-        </li>
+        <FramebufTab
+          key={key}
+          id={i}
+          onSetActiveTab={this.handleActiveClick}
+          framebuf={this.props.getFramebufByIndex(i)}
+          active={i === this.props.framebufIndex} />
       )
     })
     return (
       <div className={styles.tabHeadings}>
-        <ul className={styles.tabs}>
+        <div className={styles.tabs}>
           {lis}
-          <li><i onClick={this.handleNewTab} className='fa fa-plus'></i></li>
-        </ul>
+          <div className={styles.tab}>
+            <i onClick={this.handleNewTab} className='fa fa-plus'></i>
+          </div>
+        </div>
       </div>
     )
   }
@@ -68,7 +110,8 @@ const mapStateToProps = state => {
   const framebuf = selectors.getCurrentFramebuf(state)
   return {
     framebufIndex: selectors.getCurrentFramebufIndex(state),
-    framebufList: selectors.getFramebufs(state)
+    framebufList: selectors.getFramebufs(state),
+    getFramebufByIndex: (idx) => selectors.getFramebufByIndex(state, idx)
   }
 }
 export default connect(
