bc9f6bbd847b896c1ab75f27cd3586172d509c35
Janne Hellsten <jjhellst@gmail.com>
2018-07-26 01:03:29 +0300

    Add a place for an app icon on the top/left corner (#43)
    (left empty for now, at least until I get a better logo)

--- app/containers/App.js
+++ app/containers/App.js
@@ -5,13 +5,31 @@ import FramebufferTabs from './FramebufferTabs'
 
 import s from './App.css'
 
+class ExtLink extends Component {
+  handleIconClick = (e) => {
+    e.preventDefault()
+    require('electron').shell.openExternal(this.props.href)
+  }
+  render () {
+    return (
+      <a onClick={this.handleIconClick} href={this.props.href}>
+        {this.props.children}
+      </a>
+    )
+  }
+}
+
 export default class App extends Component {
   render() {
+    const icon = null
     return (
       <div className={s.appGrid}>
-        <div className={s.empty} />
+        <div className={s.empty}>
+          <ExtLink href='https://nurpax.github.io/petmate/'>
+            {icon}
+          </ExtLink>
+        </div>
         <div className={s.topmenu}>
-          <h2>PETSCII EDITOR</h2>
           <FramebufferTabs />
         </div>
         <div className={s.leftmenubar}>
