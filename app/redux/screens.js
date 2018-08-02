cdcb5e55861f33327afdc2824a089ffb0148c80c
Janne Hellsten <jjhellst@gmail.com>
2018-08-03 02:00:46 +0300

    Inherit previous screen colors when adding a new screen
    fixes #63

--- app/redux/screens.js
+++ app/redux/screens.js
@@ -80,11 +80,40 @@ export const actions = {
     }
   },
   newScreen: () => {
-    return {
-      type: ADD_SCREEN_AND_FRAMEBUF,
-      data: null
+    return (dispatch, getState) => {
+      const state = getState()
+      const index = state.screens.current
+      const fbidx = selectors.getScreens(state)[index]
+      const framebuf = selectors.getFramebufByIndex(state, fbidx)
+      let colors = null
+      if (framebuf !== null) {
+        colors = {
+          backgroundColor: framebuf.backgroundColor,
+          borderColor: framebuf.borderColor
+        }
+      }
+      dispatch({
+        type: ADD_SCREEN_AND_FRAMEBUF,
+        data: null
+      })
+      if (colors !== null) {
+        dispatch((dispatch, getState) => {
+          const state = getState()
+          const newScreenIdx = selectors.getCurrentScreenIndex(state)
+          const newFramebufIdx = selectors.getScreens(state)[newScreenIdx]
+          dispatch(Framebuffer.actions.setBackgroundColor(
+            colors.backgroundColor,
+            newFramebufIdx
+          ))
+          dispatch(Framebuffer.actions.setBorderColor(
+            colors.borderColor,
+            newFramebufIdx
+          ))
+        })
+      }
     }
   },
+
   setCurrentScreenIndex: (index) => {
     return {
       type: SET_CURRENT_SCREEN_INDEX,
