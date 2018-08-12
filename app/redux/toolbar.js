257b4a6adf23a63ac7bfd65259b34f7f2d66e4cf
Janne Hellsten <jjhellst@gmail.com>
2018-08-12 23:03:04 +0300

    Set default character to inverted space #83
    fixes #83

--- app/redux/toolbar.js
+++ app/redux/toolbar.js
@@ -16,7 +16,7 @@ export const TOOL_BRUSH = 3
 
 const settables = reduxSettables([
   settable('Toolbar', 'textColor', 14),
-  settable('Toolbar', 'selectedChar', {row: 0, col: 0}),
+  settable('Toolbar', 'selectedChar', {row: 8, col: 0}),
   settable('Toolbar', 'selectedTool', TOOL_DRAW),
   settable('Toolbar', 'brushRegion', null),
   settable('Toolbar', 'brush', null),
